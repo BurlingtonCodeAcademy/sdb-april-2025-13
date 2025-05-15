15b24b346d38f3fff6c315cb85aecacf7bb12c3d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-15 17:36:55 -0500

    alert and prompt

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("happy thursday!")
+
+alert("Hello! I am an alert box!!");
+
+let answer = prompt("How is the weather today?");
+
+console.log("The answer to the prompt above: ", answer);
