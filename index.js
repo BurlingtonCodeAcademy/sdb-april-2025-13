0088cb4a026fbe5eba7ed954d47104ff9795ca10
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-15 18:00:52 -0500

    form example

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 console.log("happy thursday!")
 
-alert("Hello! I am an alert box!!");
+// alert("Hello! I am an alert box!!");
 
-let answer = prompt("How is the weather today?");
+// let answer = prompt("How is the weather today?");
 
-console.log("The answer to the prompt above: ", answer);
+// console.log("The answer to the prompt above: ", answer);
+
+const form = document.getElementById("contact-form");
+console.log(form);
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const name = form.name.value;
+    const email = form.email.value;
+
+    alert(`Thank you ${name} for your submission! We will email you at ${email} with the results!`)
+
+    form.reset();
+})
